b897c42fe20d091097185806c42bcee704f6e41d
Luiz Ribeiro <luizribeiro@gmail.com>
2024-06-30 14:58:46 -0400

    added basic macro for registering handlers

---
+++ macros/src/lib.rs
@@ -0,0 +1,38 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, AttributeArgs, ItemFn, Lit, Meta, NestedMeta};
+
+#[proc_macro_attribute]
+pub fn mqtt_handler(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(attr as AttributeArgs);
+    let input = parse_macro_input!(item as ItemFn);
+
+    let mut topic = None;
+    for arg in args {
+        if let NestedMeta::Meta(Meta::NameValue(nv)) = arg {
+            if nv.path.is_ident("topic") {
+                if let Lit::Str(lit) = nv.lit {
+                    topic = Some(lit.value());
+                }
+            }
+        }
+    }
+
+    let topic = topic.expect("expected a topic");
+
+    let fn_name = &input.sig.ident;
+    let register_fn_name = format_ident!("__register_{}", fn_name);
+
+    let expanded = quote! {
+        #input
+
+        #[ctor::ctor]
+        fn #register_fn_name() {
+            crate::handlers::register_handler(#topic, #fn_name);
+        }
+    };
+
+    TokenStream::from(expanded)
+}
--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,3 +1,44 @@
+use crate::config;
+use futures::stream::AbortHandle;
+use macros::mqtt_handler;
+use regex::Regex;
+use std::sync::Mutex;
+
 pub mod start_plan;
 pub mod stop_plan;
 pub mod water_zone;
+
+type MQTTHandlerFn = fn(&mut Option<AbortHandle>, &config::Configuration, &str, &str);
+
+lazy_static! {
+    static ref HANDLERS: Mutex<Vec<(Regex, MQTTHandlerFn)>> = Mutex::new(Vec::new());
+}
+
+#[mqtt_handler(topic = "foo/bar")]
+fn foo_bar_handler(
+    _current_task_handle: &mut Option<AbortHandle>,
+    _config: &config::Configuration,
+    topic: &str,
+    payload: &str,
+) {
+    println!("foo_bar_handler: topic: {}, payload: {}", topic, payload);
+}
+
+fn register_handler(topic: &str, handler: MQTTHandlerFn) {
+    let mut handlers = HANDLERS.lock().unwrap();
+    handlers.push((Regex::new(topic).unwrap(), handler));
+}
+
+fn handle_message(
+    current_task_handle: &mut Option<AbortHandle>,
+    config: &config::Configuration,
+    topic: &str,
+    payload: &str,
+) {
+    let handlers = HANDLERS.lock().unwrap();
+    for (regex, handler) in handlers.iter() {
+        if regex.is_match(topic) {
+            handler(current_task_handle, config, topic, payload);
+        }
+    }
+}
